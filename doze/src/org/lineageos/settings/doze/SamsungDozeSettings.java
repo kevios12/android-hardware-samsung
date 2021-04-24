68104cf764be4de8ae1f6f20ae6b7fe4475941f2
kevios12 <kerschbaumer.kevin2@gmail.com>
2021-04-24 17:09:53 +0200

    Update SamsungDozeSettings.java

--- doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
+++ doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.lineageos.settings.doze;
 
 import android.app.ActionBar;
@@ -32,7 +15,6 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 
-import org.lineageos.internal.util.ScreenType;
 
 public class SamsungDozeSettings extends PreferenceFragment
         implements CompoundButton.OnCheckedChangeListener, Preference.OnPreferenceChangeListener {
@@ -82,15 +64,6 @@ public class SamsungDozeSettings extends PreferenceFragment
         }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // If running on a phone, remove padding around the listview
-        if (!ScreenType.isTablet(getContext())) {
-            getListView().setPadding(0, 0, 0, 0);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
